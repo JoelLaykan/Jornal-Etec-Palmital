d79097573ef70ec1ccba90fab11eb5785e28737c
Joel Laykan <83468175+JoelLaykan@users.noreply.github.com>
2023-03-09 12:20:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-const inicio = document.getElementById('inicio');
+function mostrarPaginaFutebol() {
+    document.getElementById("futebol").style.display = "block";
+    document.getElementById("regiaoemundo").style.display = "none";
+    document.getElementById("inicio").style.display = "block";
+}
 
-inicio.addEventListener
+function mostrarPaginaRegiaoemundo() {
+    document.getElementById("regiaoemundo").style.display = "block";
+    document.getElementById("futebol").style.display = "none";
+    document.getElementById("inicio").style.display = "block";
+}
+
+function mostrarInicio() {
+    document.getElementById("regiaoemundo").style.display = "none";
+    document.getElementById("futebol").style.display = "none";
+    document.getElementById("inicio").style.display = "block";
+}
+
+function mostrarFutebol() {
+    document.getElementById("futebol").style.display = "block";
+    document.getElementById("regiaoemundo").style.display = "none";
+    document.getElementById("inicio").style.display = "none";
+}
+
+function mostrarRegiaoemundo() {
+    document.getElementById("regiaoemundo").style.display = "block";
+    document.getElementById("futebol").style.display = "none";
+    document.getElementById("inicio").style.display = "none";
+}
